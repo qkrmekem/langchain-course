efd23894b84737e6707d0c40d57e94e253dae941
qkrmekem <magicofclown@naver.com>
2026-09-09 01:13:16 +0900

    검색 에이전트 구현 - 툴 직접 구현 및 툴 라이브러리 적용

--- main.py
+++ main.py
@@ -5,26 +5,33 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
+# from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for: {query}")
-    return "Tokyo Weather is sunny."
+# llm 모델과 마찬가지로 초기화 시점에 환경변수에서 정해진 규약에 따라 api 키를 읽어와 초기화
+# tavily = TavilyClient()
 
-llm = ChatOllama(model="qwen2.5:7b", temperature=0)
-tools = [search]
+# 검색 툴 정의(사용자 정의)
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for: {query}")
+#     return tavily.search(query = query)
+
+llm = ChatOllama(model="qwen2.5:7b", temperature=0, num_ctx=32768)
+# tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content="What's the weather in Tokyo?")})
+    result = agent.invoke({"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
     print(f"Result: {result}")
 
 if __name__ == "__main__":
